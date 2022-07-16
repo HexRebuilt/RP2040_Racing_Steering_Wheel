7b413eeb66380ebda7506973ae44f6588a7b32b1
HexRebuilt <32871843+HexRebuilt@users.noreply.github.com>
2022-07-16 22:13:34 +0200

    added the two subfolders

---
+++ Pedal_Box/Pedal_Box_RP2040/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <Arduino.h>
+#include <string.h>
+#include <stdlib.h>
+#include <SPI.h>
+#include <TinyGPS++.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
