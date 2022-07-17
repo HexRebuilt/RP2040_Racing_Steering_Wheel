7d9a1f4e9530a7f30aca15ba05d31d2639174e58
HexRebuilt <32871843+HexRebuilt@users.noreply.github.com>
2022-07-17 10:13:44 +0200

    serial definition

---
+++ Pedal_Box/Pedal_Box_RP2040/src/defines.h
@@ -0,0 +1,15 @@
+//GPS serial defines
+#define GPSBAUD 9600
+#define GPSTX   8
+#define GPSRX   9
+
+//Wheel serial defines
+#define WHEELBAUD 9600
+#define WHEELTX   10
+#define WHEELRX   11
+
+//radio potentiometer
+#define RADIO_MPC4131_CS            27
+#define RADIO_MPC4131_ADDRESS       0x00
+#define POT_MIN                     0
+#define POT_MAX                     127
--- Pedal_Box/Pedal_Box_RP2040/src/main.cpp
+++ Pedal_Box/Pedal_Box_RP2040/src/main.cpp
@@ -1,16 +1,42 @@
 #include <Arduino.h>
-#include <Arduino.h>
-#include <string.h>
 #include <stdlib.h>
+#include <hardware/pio.h>
 #include <SPI.h>
 #include <TinyGPS++.h>
+#include <string.h>
+
+#include "defines.h"
+
+unsigned short radioOutputStep = 0;
+
+UART SerialGPS (GPSTX, GPSRX, NC, NC); 
+UART SerialWheel (WHEELTX, WHEELRX, NC, NC); 
 
 
+TinyGPSPlus gps;
 
 void setup() {
   // put your setup code here, to run once:
+  //DEBUG OVER USB serial
+  Serial.begin(9600);
+
+  //starting the serial communication to the wheel
+  SerialWheel.begin(9600);
+  Serial.println("Communication to the wheel started...");
+
+  //gps configuration
+  SerialGPS.begin(GPSBAUD);
+  Serial.println("GPS STARTED");
+
+  Serial.println("Configuration DONE");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  //GPS reading info
+  while (SerialGPS.available()){
+    gps.encode(SerialGPS.read());
+  }
+  
 }
